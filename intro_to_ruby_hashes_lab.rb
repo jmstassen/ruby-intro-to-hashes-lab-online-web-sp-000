1cb2c4af55f1efe609801f8e4d1509b52821b537
John Stassen <john.stassen@gmail.com>
2020-08-22 20:36:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,8 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads =>
-    {:pieces => 4},{}
-    }
+  monopoly = {:railroads => {}}
+
 end
 
 def monopoly_with_third_tier
